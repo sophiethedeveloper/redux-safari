6c742bfec903f002020c608b184c3acf5b9c67cf
sofia Luna <Sofia21luna@gmail.com>
2020-10-15 19:50:33 -0600

    login is done

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-export default function Login(props) {
-    // How can we log in? What do we need to do?
+const Login = (props) => {
+  // How can we log in? What do we need to do?
 
-    return (
-        <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
-        </div>
-    )
-}
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", login)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/creatures')
+      })
+      .catch((err) => console.log(`Login axiosWithAuth Failed: ${err.response}`));
+  };
+
+  const handleChange = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <h1 className="adventure">Let's see some animals that could eat us!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={props.username}
+          onChange={handleChange}
+          placeholder="username"
+          className="input"
+        />
+        <input
+          type="password"
+          name="password"
+          value={props.password}
+          onChange={handleChange}
+          placeholder="password"
+          className="input"
+        />
+        <button>Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
