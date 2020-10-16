fa5aa204e015246f26c94562cdd8842e95b320c7
sofia Luna <Sofia21luna@gmail.com>
2020-10-15 18:45:48 -0600

    axiosWithAuth file

---
+++ animals/src/utils/axiosWithAuth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import axios from 'axios';
+
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token')
+
+    return axios.create({
+        baseURL: 'http://localhost:5000',
+        headers: {
+            Authorization: token
+        }
+    })
+}
