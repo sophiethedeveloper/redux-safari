9bb6650f9feab8aaee2d3f08f60155aa4cbc6025
sofia Luna <Sofia21luna@gmail.com>
2020-10-15 20:04:48 -0600

    Animal Dashboard is displaying animals

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
-
+import PrivateRoute from './utils/PrivateRoute';
 import Login from "./components/Login.js";
 import Header from "./components/Header.js";
+import AnimalDashboard from './components/AnimalDashboard';
+
 
 export default function App() {
 
@@ -12,6 +14,7 @@ export default function App() {
       <Header />
       <Switch>
         {/* Build out a Private Route */}
+        <PrivateRoute exact path="/creatures" component={AnimalDashboard}/>
         <Route exact path="/login" component={Login} />
       </Switch>
     </div>
--- animals/src/components/AnimalDashboard.js
+++ animals/src/components/AnimalDashboard.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import AnimalForm from "./AnimalForm.js";
 import AnimalList from "./AnimalsList.js";
 
 export default function AnimalDashboard() {
-    
-    const [ animals, setAnimals ] = useState([]);
-    
-    // How can get fetch the data from the server when the component mounts?
-    // How can we capture and set that data to state?
+  const [animals, setAnimals] = useState([]);
 
-    return(
-        <div className="dash">
-            <AnimalForm animals={animals} updateAnimals={setAnimals} />
-            <AnimalList animals={animals} />
-        </div>
-    )
+  // How can get fetch the data from the server when the component mounts?
+  // How can we capture and set that data to state?
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/animals")
+      .then((res) => {
+        setAnimals(res.data);
+      })
+      .catch((err) => console.log("cannot display animals", err.response));
+  }, []);
+
+  return (
+    <div className="dash">
+      <AnimalForm animals={animals} updateAnimals={setAnimals} />
+      <AnimalList animals={animals} />
+    </div>
+  );
 }
